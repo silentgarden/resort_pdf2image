3ad006f77e541e6d3279337a78cb6717d0cfd03e
Silent Garden <silentgarden837@gmail.com>
2023-11-09 18:03:15 +0530

    changing to pdf tools

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -1,18 +1,3 @@
-import PySimpleGUI as sg
+from pdf2image import convert_from_path
 
-#######################
-#Window Initialization#
-#######################
-
-sg.theme('LightBrown7')
-
-extensions = ['png', 'jpg']
-layout = [[sg.Input("", font=('Times\ New\ Roman 16'), key='-INPUT-')], [sg.FileBrowse('Select', font=('Georgia 18'), file_types=(('PDF Files', '*.pdf'),))], [sg.Combo(extensions, key="inp", disabled=True, default_value="png", enable_events= True, font=('Arial 18')), sg.Button('Convert', disabled=True, font=('Arial 17'), enable_events=True)]]
-
-window = sg.Window('PDF to Image', layout, size=(370, 160), resizable=False, element_justification='c')
-
-while True:
-	event, values = window.read()
-	if event == sg.WIN_CLOSED or event == 'Cancel':
-		break
-
+images = convert_from_path('C:\\Users\\Admin\\Documents\\card-new.pdf', output_folder="C:\\Users\\Admin\\Documents")
