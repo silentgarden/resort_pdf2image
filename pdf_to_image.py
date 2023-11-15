27a3e4cc9262b19f5fc8819caef16c26b4709a97
Silent Garden <silentgarden837@gmail.com>
2023-11-15 16:11:05 +0530

    pdf2image added

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -1,3 +1,5 @@
 from pdf2image import convert_from_path
 
-images = convert_from_path('C:\\Users\\Admin\\Documents\\card-new.pdf', output_folder="C:\\Users\\Admin\\Documents")
+images = convert_from_path('example2.pdf')
+
+images[0].save('pagex'+'.jpg', 'JPEG')
